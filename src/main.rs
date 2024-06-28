a0ce09597b60f54386129085091d10b95215b2ca
ksks2012 <st890751999@gmail.com>
2024-06-28 11:37:51 +0800

    feat(main): Add example using sdsrange

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ fn main() {
     println!("After append ', world!': {:?}", sds);
     println!("SDS content: {}", sds.to_string());
 
+    let range = sds.sdsrange(1, 4);
+    println!("SDS range from 1 to 4: {:?}", range);
+    println!("SDS content: {:?}", sds.to_string());
+
     sds.sdsclear();
     println!("After clear: {:?}", sds);
     println!("SDS is empty: {}", sds.sdsempty());
